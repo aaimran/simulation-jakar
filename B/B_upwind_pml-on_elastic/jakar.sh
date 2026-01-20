97a49ee10c9ddbbac2951878bf857857a807d3c0
aimran <aimran@miners.utep.edu>
2026-01-20 11:24:34 -0700

    added pml-12

---
+++ B/B_upwind_pml-on_elastic/jakar.sh
@@ -0,0 +1,34 @@
+# Default values
+nprocs=40
+fname="upwind_6_pml-on_elastic"
+
+# upwind_6_pml-on_attenuation-c2
+# upwind_6_pml-off_attenuation-c2
+# upwind_6_pml-on_elastic
+# upwind_6_pml-off_elastic
+
+# Parse named arguments
+for arg in "$@"; do
+    case $arg in
+        np=*)
+            nprocs="${arg#*=}"
+            ;;
+        fname=*)
+            fname="${arg#*=}"
+            ;;
+        *)
+            echo "Unknown argument: $arg"
+            echo "Usage: $0 [np=<number>] [fname=<filename>]"
+            echo "Example: $0 np=8 fname=WholeSpace_external"
+            exit 1
+            ;;
+    esac
+done
+
+
+module purge
+module load compiler-rt/2024.0.0 ifort/2024.0.0 mpi/2021.13 R
+
+mpirun -np $nprocs ../../build/./waveqlab3d ${fname}.in | tee ${fname}.out
+
+
